1dea96fa706c3cb0e40a8482b1048976529612e7
Naira Yeghiazaryan <60625888+NairaYeg@users.noreply.github.com>
2020-06-28 15:50:36 +0400

    Update getRandomLetter.js
    fixed getRandomLetter() function

--- getRandomLetter.js
+++ getRandomLetter.js
@@ -1,12 +1,15 @@
 function getRandomLetter() {
-    let char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    // console.log(char.length)
-    return char[Math.floor(Math.random() * 51)];
-  }
-  
-  getRandomLetter();
-  getRandomLetter();
-  getRandomLetter();
+  let char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  let max = 51;
+  let min = 0;
+  return char[Math.floor(Math.random() * (max - min + 1) + min)];
+}
+
+console.log(getRandomLetter());
+
+getRandomLetter();
+getRandomLetter();
+getRandomLetter();
 
 
   
