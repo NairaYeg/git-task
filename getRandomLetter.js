72a10cc8ecbf43fd334cdcb7fed7b96364f46bdb
Naira Yeghiazaryan <yeghiazaryannaira97@gmail.com>
2020-06-28 14:35:19 +0400

    added getRandomLetter() function

---
+++ getRandomLetter.js
@@ -0,0 +1,6 @@
+function getRandomLetter() {
+    let char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    // console.log(char.length)
+    return char[Math.floor(Math.random() * 51)];
+  }
+  
